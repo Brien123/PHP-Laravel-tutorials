c688c68f224196b3328b34e42dac439bf86851d5
Brien123 <zbrine1335@gmail.com>
2024-06-06 16:25:22 +0100

    oop

--- helloworld.php
+++ helloworld.php
@@ -209,6 +209,193 @@
     array_push($cars, 'ford');
     var_dump($cars);
     echo '<br>';
+
+    // removing items from an array
+    //using array_splice
+    array_splice($cars, 1,1);
+    var_dump($cars);
+    echo '<br>';
+
+    // using unset function
+    $cars = array("Volvo", "BMW", "Toyota");
+    unset($cars[1]);
+    var_dump($cars);
+    echo '<br>';
+
+    // removing the last item
+    $cars = array("Volvo", "BMW", "Toyota");
+    array_pop($cars);
+    var_dump($cars);
+    echo '<br>';
+
+    // removing first item
+    $cars = array("Volvo", "BMW", "Toyota");
+    array_shift($cars);
+    var_dump($cars);
+    echo '<br>';
+
+    // sorting arrays
+    $cars = array("Volvo", "BMW", "Toyota");
+    sort($cars);
+    var_dump($cars);
+    echo '<br>';
+
+
+    // Regular Expressions(Regex)
+    // Using preg_match()
+    $str = "Visit W3Schools";
+    $pattern = "/w3schools/i";
+    echo preg_match($pattern, $str);
+    echo '<br>';
+
+    // Using preg_match_all()
+    $str = "The rain in SPAIN falls mainly on the plains.";
+    $pattern = "/ain/i";
+    echo preg_match_all($pattern, $str);
+    echo '<br>';
+
+    // Using preg_replace()
+    $str = "Visit Microsoft!";
+    $pattern = "/microsoft/i";
+    echo preg_replace($pattern, "W3Schools", $str);
+    echo '<br>';
+
+    // PHP if/else loops
+    if (5>3){
+        echo "hello world";
+    }
+    echo '<br>';
+
+    // if else
+    $x = 10;
+    if ($x>11){
+        echo 'hello';
+    }
+    else {
+        echo 'world';
+    }
+    echo '<br>';
+
+    // logics
+    $a = 5;
+
+    if ($a == 2 || $a == 3 || $a == 4 || $a == 5 || $a == 6 || $a == 7) {
+        echo "$a is a number between 2 and 7";
+    }
+    echo '<br>';
+
+    // switch statement
+    $col = 'green';
+    switch ($col) {
+        case "red":
+          echo "Your favorite color is red!";
+          break;
+        case "blue":
+          echo "Your favorite color is blue!";
+          break;
+        case "green":
+          echo "Your favorite color is green!";
+          break;
+        default:
+          echo "Your favorite color is neither red, blue, nor green!";
+      }
+      echo '<br>';
+
+    // while loop
+    $i = 1;
+    while($i<10){
+        echo $i;
+        $i++;
+    }
+    echo '<br>';
+
+    // break statement in while loop
+    $i = 1;
+    while($i<10){
+        if($i==5) break;// there is also a continue statement
+        echo $i;
+        $i++;
+    }
+    echo '<br>';
+
+    // do while loop
+    $i = 1;
+    do {
+        echo $i;
+        $i++;
+    } while ($i<5);
+
+    // for loop
+    for ($x=0; $x<=10; $x++){
+        echo "The number is: $x <br>"; 
+    }
+
+    // foreach loop
+    $colors = array("red", "green", "blue", "yellow");
+
+    foreach ($colors as $x) {
+        echo "$x <br>";
+    }
+
+    // php functions
+    function hello(){
+        echo "Hello world<br>";
+    }
+    hello();
+
+    // sum
+    function sum($x, $y) {
+        $z = $x + $y;
+        return $z;
+      }
+      
+    echo "5 + 10 = " . sum(5, 10) . "<br>";
+
+    // Php oop
+    // classes and objects
+    // Defining a class
+    class fruit {
+        // properties
+        public $name;
+        public $color;
+
+        //methods
+        function set_name($name) {
+            $this->name = $name;
+        }
+
+        function get_name() {
+            return $this->name;
+        }
+
+        function set_color($color){
+            $this->color = $color;
+        }
+
+        function get_color(){
+            return $this->color;
+        }
+    }
+
+    // Defining objects
+    $apple = new fruit();
+    $banana = new fruit();
+    $apple->set_name('Apple');
+    $banana->set_name('Banana');
+    $apple->set_color('Red');
+    $banana->set_color('Yellow');
+
+    echo $apple->get_name();
+    echo '<br>';
+    echo 'The color of the apple is ', $apple->get_color();
+    echo '<br>';
+
+    echo $banana->get_name();
+    echo '<br>';
+    echo 'The color of the banana is ', $banana->get_color();
+    echo '<br>';
+
+    
     ?>
 </body>
 </html>
